23aac12b2b32684006343fe0ab7222b05006da37
atul <atulgupta.d796@gmil.com>
2024-02-22 07:35:33 +0530

    22-2-24

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 // Use the chrome.runtime API instead of chrome.extension
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    const logoutButton = document.querySelector('.pt-2'); // Replace with the actual selector
+    const logoutButton = document.querySelector('.notBtn'); // Replace with the actual selector
   
     if (logoutButton) {
       logoutButton.addEventListener('click', function () {
-        alert('Logout button clicked!');  // You can customize this alert message
+        alert('Notification button clicked!');  // You can customize this alert message
       });
     }
   });
